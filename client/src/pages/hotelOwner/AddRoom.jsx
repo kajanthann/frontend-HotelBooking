2f3104190b57ba485cb98fe45a2006021243e3ba
kajanthann <arulkajanthan904@gmail.com>
2025-04-28 23:09:16 +0530

    completed all

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/Title'
+import { assets } from '../../assets/assets'
 
 const AddRoom = () => {
+
+  const [img,setImg] = useState({
+    1:null,
+    2:null,
+    3:null,
+    4:null,
+  })
+
+  const [input,setInput] = useState({
+    roomType: '',
+    pricePerNight:0,
+    amenities: {
+      'Free wifi': false,
+      'Free breakfast': false,
+      'room service': false,
+      'mountin view': false,
+      'fool access': false,
+    }
+  })
+
   return (
-    <div>AddRoom</div>
+    <form>
+      <Title align='left' title='Add Room' font='outfit' subtitle='fill in the details carefully and accurate room details, pricing. and amenitis, to enhance the use booking experience.' />
+
+      {/* upload img */}
+      <p className='text-gray-800 mt-10'>Images</p>
+      <div className='grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap'>
+        {Object.keys(img).map((key) => (
+          <label className='' htmlFor={`roomImage${key}`} key={key}>
+            <img className='max-h-13 cursor-pointer opacity-80' src={img[key] ? URL.createObjectURL(img[key]) : assets.uploadArea} alt="" />
+            <input type="file" accept='image/*' name="" id={`roomImage${key}`} hidden 
+            onChange={e => setImg({...img,[key]: e.target.files[0]})}/>
+          </label>
+        ))}
+      </div>
+
+      <div className='w-full flex max-sm:flex-col sm:gap-4 mt-4'>
+        <div className='flex-1 max-w-48'>
+          <p className='text-gray-800 mt-4'>Room Type</p>
+          <select value={input.roomType} onChange={e => setInput({...input,roomType: e.target.value})} className='border opacity-70 border-gray-300 mt-1 rounded p-2 w-full'>
+            <option value="">Slelect Room Type</option>
+            <option value="Single Bed">Single Bed</option>
+            <option value="Double Bed">double Bed</option>
+            <option value="Luxury Room">Luxury Room</option>
+            <option value="Family Suite">Family Suite</option>
+          </select>
+        </div>
+        <div>
+          <p className='mt-4 text-gray-800'>Price <span className='text-sm'>/night</span> </p>
+          <input className='border border-gray-300 mt-1 rounded p-2 w-24' type="number" placeholder='0' value={input.pricePerNight} onChange={e => setInput({...input,pricePerNight: e.target.value})} id="" />
+        </div>
+      </div>
+
+      <p className='text-gray-800 mt-4'>Aminites</p>
+      <div className='flex flex-col max-w-sm flex-wrap mt-1 text-gray-400'>
+        {Object.keys(input.amenities).map((aminity,index) => (
+          <div key={index}>
+            <input type="checkbox" id={`amenities${index+1}`} checked={input.amenities[aminity]}
+            onChange={() => setInput({...input,amenities: {...input.amenities, [aminity]: !input.amenities[aminity]}})} />
+            <label className='ml-2' htmlFor={`amenities${index+1}`}>{aminity}</label>
+          </div>
+        ))}
+      </div>
+      <button className='bg-primary text-white px-8 py-2 mt-8 cursor-pointer'>Add Room</button>
+    </form>
   )
 }
 
--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import { roomsDummyData } from "../../assets/assets";
+import Title from "../../components/Title";
 
 const ListRoom = () => {
+  const [rooms, setRooms] = useState(roomsDummyData);
   return (
-    <div>ListRoom</div>
-  )
-}
+    <div>
+      <Title
+        align="left"
+        font="outfit"
+        title="Room Listings"
+        subtitle="hhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhh h         hirhfrhf uht894 ytuti4[u tv984u[t"
+      />
+      <p className="mt-8 text-gray-800">All Rooms</p>
+      <div className="w-full max-w-3xl mt-3 text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 px-4 text-gray-800 font-medium">Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">
+                Facility
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium">
+                pricePerNight
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Action
+              </th>
+            </tr>
+          </thead>
 
-export default ListRoom
+          <tbody className="text-sm">
+            {
+              rooms.map((item,index) => (
+                <tr key={index}>
+                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                    {item.roomType}
+                  </td>
+                  <td className="py-3 px-4 max-sm:hidden text-gray-700 border-t border-gray-300">
+                    {item.amenities.join(', ')}
+                  </td>
+                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                    {item.pricePerNight}
+                  </td>
+                  <td className="py-3 px-4 text-center text-sm text-red-500 border-t border-gray-300">
+                    <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+                      <input type="checkbox" className="sr-only peer" checked={item.isAvailable} name="" id="" />
+                      <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200">
+
+                      </div>
+                      <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                    </label>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListRoom;
